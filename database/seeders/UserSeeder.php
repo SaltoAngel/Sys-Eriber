09fc7a8c719d40bfe7211dd0a2404b1505efce2b
DaniTsu <danielelpro19@gmail.com>
2026-04-03 02:24:43 -0400

    fix:me arrepenti

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,26 +2,18 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        DB::table('users')->insert([
-            'id' => 1,
-            'name' => 'admin',
-            'email' => 'admin@softui.com',
-            'password' => Hash::make('secret'),
-            'created_at' => now(),
-            'updated_at' => now()
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@gmail.com',
+            'password' => '12345678',
         ]);
     }
 }
