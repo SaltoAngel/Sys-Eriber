522bd6fa112187529c189a5ba06ed4fd7b494f91
DaniTsu <danielelpro19@gmail.com>
2026-04-03 02:27:04 -0400

    ya po

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    //
+    public function index() {
+        return view('pages.auth.login');
+    }
+
+    public function olvidoContrasena() {
+        return view('pages.auth.olvido-contrasena');
+    }
 }
