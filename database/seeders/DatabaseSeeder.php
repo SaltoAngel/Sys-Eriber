d1d4c21fab6825ab491a07199bed847f00642ffc
DaniTsu <danielelpro19@gmail.com>
2026-04-03 02:05:21 -0400

    feat: al carajo, te desacrgue una plantilla entera

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
+        // \App\Models\User::factory(10)->create();
         $this->call([
-            UserSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,18 +2,26 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    public function run(): void
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
     {
-        User::create([
-            'name' => 'Administrador',
-            'email' => 'admin@gmail.com',
-            'password' => '12345678',
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'email' => 'admin@softui.com',
+            'password' => Hash::make('secret'),
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
     }
 }
